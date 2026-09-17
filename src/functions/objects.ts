a73b20c5b338af22a6ba0eba261011247fb2a9bd
quantumblin <gogasarus@gmail.com>
2026-09-17 13:32:25 +0300

    Добавил функцию modify object

--- src/functions/objects.ts
+++ src/functions/objects.ts
@@ -1,6 +1,14 @@
 import type { Slide, Background } from "../types/slide.js";
-import type { Filter, Animation, Size, Point, TextStyle, FigureStyle, textObjectArgs, imageObjectArgs, figureObjectArgs } from "../types/objects.js";
+import type { Filter, Animation, Size, Point, TextStyle, FigureStyle, textObjectArgs, imageObjectArgs, figureObjectArgs, SlideObject } from "../types/objects.js";
 
+function modifyObject(slide: Slide, objectId: string, modifyFn: (obj: SlideObject) => SlideObject): Slide {
+    return {
+        ...slide,
+        objects: slide.objects.map(obj => 
+            obj.id === objectId ? modifyFn(obj) : obj
+        )
+    }
+}
 
 //TODO: Отдельный тип для параметров args
 function addTextObject(slide: Slide, {id, spans, position, size, textLayout}: textObjectArgs): Slide {
@@ -52,85 +60,60 @@ function removeObject(slide: Slide, objectId: string): Slide {
 }
 
 function moveObject(slide: Slide, objectId: string, newPosition: Point): Slide {
-    return {
-        ...slide,
-        objects: slide.objects.map(obj => {
-            if (obj.id === objectId) {
-                return {
-                    ...obj,
-                    position: newPosition
-                }
-            }
-            return obj
-        })
-    }
+    return modifyObject(slide, objectId, obj => ({
+        ...obj,
+        position: newPosition
+    }));
 }
 
 function resizeObject(slide: Slide, objectId: string, newSize: Size): Slide {
-    if (newSize.width <= 0 || newSize.width <= 0) {
-        return slide
+    if (newSize.width <= 0 || newSize.height <= 0) {
+        return slide;
     }
 
-    return {
-        ...slide,
-        objects: slide.objects.map(obj => {
-            if (obj.id === objectId) {
-                return {
-                    ...obj,
-                    size: newSize
-                }
-            }
-            return obj
-        })
-    }
+    return modifyObject(slide, objectId, obj => ({
+        ...obj,
+        size: newSize
+    }));
 }
 
 function updateTextObjectStyle(slide: Slide, objectId: string, newTextStyle: TextStyle): Slide {
-    return {
-        ...slide,
-        objects: slide.objects.map(obj => {
-            if (obj.id === objectId && obj.type === 'text') {
-                return {
-                    ...obj,
-                    spans: obj.spans.map(span => ({
-                        ...span,
-                        style: newTextStyle
-                    }))
-                }
-            }
-            return obj
-        })
-    }
+    return modifyObject(slide, objectId, obj => {
+        if (obj.type === 'text') {
+            return {
+                ...obj,
+                spans: obj.spans.map(span => ({
+                    ...span,
+                    style: newTextStyle
+                }))
+            };
+        }
+        return obj;
+    });
 }
 
 function updateImageObjectStyle(slide: Slide, objectId: string, newFilter: Filter){
-    return {
-        ...slide,
-        objects: slide.objects.map(obj => {
-            if (obj.id === objectId) {
-                return {
-                    ...obj,
-                    filters: newFilter
-                }
-            }
-            return obj
-        })
-    }    
+    return modifyObject(slide, objectId, obj => {
+        if (obj.type === 'image') {
+            return {
+                ...obj,
+                filters: newFilter
+            };
+        }
+        return obj;
+    });
 }
 
 function updateFigureObjectStyle(slide: Slide, objectId: string, newFigureStyle: FigureStyle) {
-    return {
-        ...slide,
-        objects: slide.objects.map(obj => {
-            if (obj.id === objectId) {
-                return {
-                    ...obj,
-                    figureStyle: newFigureStyle
-                }
-            }
-            return obj
-        })
-    }        
+    return modifyObject(slide, objectId, obj => {
+        if (obj.type === 'figure') {
+            return {
+                ...obj,
+                figureStyle: newFigureStyle
+            };
+        }
+        return obj;
+    });   
 }
 
 export {
